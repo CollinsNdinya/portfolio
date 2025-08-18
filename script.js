2c549ad20167f0b8974420de253d8e42996e94ec
Collins Ndinya <collinsonyango635@gmail.com>
2025-08-19 00:18:30 +0300

    Changes after redifining the project idea

---
+++ script.js
@@ -0,0 +1,64 @@
+
+function contactPage() {
+  const url = 'mailto:collinsonyango635@gmail.com';
+
+  window.open(url, "_blank");
+
+}
+
+function submitted() {
+     alert("Submission succesful!");
+}
+
+//function to compose mail
+const mailsvg = document.getElementById('mailsend');
+
+mailsvg.addEventListener('click', () => {
+  
+  const url = 'mailto:collinsonyango635@gmail.com';
+
+  window.open(url, '_blank');
+});
+
+
+//function to open facebook url
+const xsvg = document.getElementById('x');
+
+xsvg.addEventListener('click', () => {
+  
+  const url = 'https://x.com/_lakesidetech?t=mIZm-tIC66vh5mbUp-AOcw&s=09';
+
+  window.open(url, '_blank');
+});
+
+
+//function to open instagram url
+const igsvg = document.getElementById('instagram');
+
+igsvg.addEventListener('click', () => {
+  
+  const url = 'https://www.instagram.com/lakeside.tech?igsh=eDVwM250a2w0cGw2';
+
+  window.open(url, '_blank');
+});
+
+//function to open facebook url
+const fbsvg = document.getElementById('facebook');
+
+fbsvg.addEventListener('click', () => {
+  
+  const url = 'https://www.facebook.com/profile.php?id=61552142864543 ';
+
+  window.open(url, '_blank');
+});
+
+const url = 'https://sv443.net/jokeapi/v2/joke/Any'
+
+async function getData() {
+  const response = await fetch(url)
+  const data = await response.json()
+  console.log(data)
+}
+
+getData()
+
